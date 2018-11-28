3dfd3152eda45a252fe10a8bb2b91e765a543872
Tom Elliott <tomelliottnz@gmail.com>
2018-11-28 23:38:53 +1300

    Custom module class, which will be used to add/remove modules (#57)

---
+++ R/CustomModule.R
@@ -0,0 +1,27 @@
+CustomModule <- setRefClass(
+    "CustomModule",
+    fields = list(
+        GUI = "ANY",
+        moduledir = "character"
+    ),
+    methods = list(
+        initialize = function(gui) {
+            initFields(GUI = gui)
+
+            ## set the moduledirectory
+            # moduledir <<- file.path("~", "Documents", "iNZightVIT", "modules")
+
+            ## check for and load any existing modules
+            # mods <- listModules(moduledir)
+
+        }
+    )
+)
+
+#' @export 
+getModules <- function(load) {
+    dir <- file.path("~", "Documents", "iNZightVIT", "modules")
+    mods <- list.files(dir, full.names = TRUE)
+    if (load) sapply(mods, source)
+    basename(mods)
+}
