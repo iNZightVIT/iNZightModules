20b264993ae32e2b20b340fdd4f5517e70c0c184
Tom Elliott <tomelliottnz@gmail.com>
2020-07-17 09:22:15 +1200

    Fix logic

--- R/ModuleManager.R
+++ R/ModuleManager.R
@@ -31,7 +31,7 @@ ModuleManager <- setRefClass(
                 .confirm = confirm
             )
 
-            if (!file.exists(m_dir) && !interactive()) {
+            if (!file.exists(m_dir) && interactive()) {
                 do.create <- gconfirm(
                     paste0(
                         "You need to create a modules directory to install modules into.\n\n",
