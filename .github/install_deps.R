6ce98565ede0d045f9193226f967fb674a5a3bbe
Tom Elliott <tomelliottnz@gmail.com>
2020-07-26 09:36:40 +1200

    Install dependencies correctly (GithubCI)

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -1,24 +1,25 @@
 # R script
 github_deps <- c(
     "iNZightVIT/gWidgets2RGtk2@inz",
-    "iNZightVIT/iNZightTools@release/1.9",
-    "iNZightVIT/iNZightTS@dev",
-    "iNZightVIT/iNZightMR@dev",
-    "iNZightVIT/iNZightPlots@release/2.12",
-    "iNZightVIT/iNZight@release/4.0"
+    "iNZightVIT/iNZightTools@1.9",
+    "iNZightVIT/iNZightTS@1.5.4",
+    "iNZightVIT/iNZightMR@2.2.5",
+    "iNZightVIT/iNZightPlots@2.12",
+    "iNZightVIT/iNZightRegression@1.2.8",
+    "iNZightVIT/iNZight@4.0"
 )
 
-options(repos = c(CRAN = "https://cloud.r-project.org"))
+OS <- Sys.getenv("OS_TYPE")
+options(
+    repos = c(
+        if (OS == "Linux") RSPM = Sys.getenv("RSPM"),
+        CRAN = "https://cloud.r-project.org"
+    ),
+    install.packages.compile.from.source = "never"
+)
 
-if (.Platform$OS.type == "windows") {
-    if (!requireNamespace("XML", quietly = TRUE))
-        utils::install.packages("XML", type = "binary")
-
-    if (!requireNamespace("cairoDevice", quietly = TRUE))
-        utils::install.packages("cairoDevice", type = "binary")
-
-    if (!requireNamespace("RODBC", quietly = TRUE))
-        utils::install.packages("RODBC", type = "binary")
+if (OS == "Windows" && getRversion() < numeric_version("4")) {
+    install.packages("RODBC", type = "binary")
 }
 
 remotes::install_github(
