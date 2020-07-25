0cd1508b7d3815623905c02fc98dc53e7e0613ac
Tom Elliott <tomelliottnz@gmail.com>
2020-07-25 13:11:51 +1200

    Switch to using GH actions

---
+++ .github/install_deps.R
@@ -0,0 +1,32 @@
+# R script
+github_deps <- c(
+    "iNZightVIT/gWidgets2RGtk2@inz",
+    "iNZightVIT/iNZightTools@release/1.9",
+    "iNZightVIT/iNZightTS@dev",
+    "iNZightVIT/iNZightMR@dev",
+    "iNZightVIT/iNZightPlots@release/2.12",
+    "iNZightVIT/iNZight@release/4.0"
+)
+
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+if (.Platform$OS.type == "windows") {
+    if (!requireNamespace("XML", quietly = TRUE))
+        utils::install.packages("XML", type = "binary")
+
+    if (!requireNamespace("cairoDevice", quietly = TRUE))
+        utils::install.packages("cairoDevice", type = "binary")
+
+    if (!requireNamespace("RODBC", quietly = TRUE))
+        utils::install.packages("RODBC", type = "binary")
+}
+
+remotes::install_github(
+    github_deps,
+    INSTALL_opts = "--no-multiarch"
+)
+remotes::install_deps(
+    dependencies = TRUE,
+    INSTALL_opts = "--no-multiarch"
+)
+remotes::install_cran("rcmdcheck")
