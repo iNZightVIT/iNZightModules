a94fb9e6cb878dbc214578cd636e74a389219ec5
Tom Elliott <tomelliottnz@gmail.com>
2020-07-25 14:39:45 +1200

    Missing keyword

--- R/ModuleManager.R
+++ R/ModuleManager.R
@@ -31,7 +31,7 @@ ModuleManager <- setRefClass(
                 .confirm = confirm
             )
 
-            if (!file.exists(m_dir) && interactive()) { # nocov
+            if (!file.exists(m_dir) && interactive()) { # nocov start
                 do.create <- gconfirm(
                     paste0(
                         "You need to create a modules directory to install modules into.\n\n",
