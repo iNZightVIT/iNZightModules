376ff535fd4adfc55d91bec155079999d9506066
Tom Elliott <tomelliottnz@gmail.com>
2022-11-03 14:13:38 +1300

    install dev surveyspec

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -1,6 +1,7 @@
 # R script
 github_deps <- c(
     "gdemin/expss",
+    "tmelliott/surveyspec@develop",
     "tmelliott/gWidgets2@patch-1",
     "iNZightVIT/gWidgets2RGtk2@inz",
     "iNZightVIT/iNZightTools@1.12.1",
