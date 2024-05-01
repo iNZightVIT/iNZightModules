3770f1c9b1d392a6b5c7b13c0e7864cd5f211543
Tom Elliott <tomelliottnz@gmail.com>
2024-05-02 10:13:31 +1200

    update ci

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -2,6 +2,8 @@
 github_deps <- c(
     "tmelliott/surveyspec",
     "tmelliott/gWidgets2@patch-1",
+    "cran/maptools",
+    "cran/rgeos",
     "iNZightVIT/gWidgets2RGtk2@inz",
     "iNZightVIT/iNZightTools@1.13.0",
     "iNZightVIT/iNZightTS@1.5.8",
@@ -21,35 +23,42 @@ options(
     repos = c(
         if (OS == "Linux") RSPM <- Sys.getenv("RSPM"),
         CRAN = "https://cloud.r-project.org"
-    ),
-    install.packages.compile.from.source = "never"
+    )
 )
 
-if (OS == "Linux" &&
-    !requireNamespace("units", quietly = TRUE) &&
-    getRversion() >= "4.3") {
-    system("sudo apt-get install libudunits2-dev")
-}
+# if (OS == "Linux" &&
+#     !requireNamespace("units", quietly = TRUE) &&
+#     getRversion() >= "4.3") {
+#     system("sudo apt-get install libudunits2-dev")
+# }
 
-if (OS == "Windows" && getRversion() < numeric_version("4")) {
-    install.packages("RODBC", type = "binary")
-    install.packages("rgl", type = "binary")
-}
-if (OS == "Windows") {
-    dir.create(tools::R_user_dir("iNZight", "config"), recursive = TRUE)
-}
+# if (OS == "Windows" && getRversion() < numeric_version("4")) {
+#     install.packages("RODBC", type = "binary")
+#     install.packages("rgl", type = "binary")
+# }
+# if (OS == "Windows") {
+#     dir.create(tools::R_user_dir("iNZight", "config"), recursive = TRUE)
+# }
 
 # install broom.helpers if not already installed
-if (!requireNamespace("broom.helpers", quietly = TRUE)) {
-    install.packages("broom.helpers", repos = "https://cloud.r-project.org")
+# if (!requireNamespace("broom.helpers", quietly = TRUE)) {
+#     install.packages("broom.helpers", repos = "https://cloud.r-project.org")
+# }
+
+# remotes::install_github(
+#     github_deps,
+#     INSTALL_opts = "--no-multiarch"
+# )
+# remotes::install_deps(
+#     dependencies = TRUE,
+#     INSTALL_opts = "--no-multiarch"
+# )
+# remotes::install_cran("rcmdcheck")
+
+if (!requireNamespace("pak", quietly = TRUE)) {
+    install.packages("pak", type = "source")
 }
 
-remotes::install_github(
-    github_deps,
-    INSTALL_opts = "--no-multiarch"
-)
-remotes::install_deps(
-    dependencies = TRUE,
-    INSTALL_opts = "--no-multiarch"
-)
-remotes::install_cran("rcmdcheck")
+pak::pak(github_deps, dependencies = TRUE)
+pak::local_install_dev_deps(dependencies = TRUE, upgrade = FALSE)
+pak::pak("rcmdcheck")
