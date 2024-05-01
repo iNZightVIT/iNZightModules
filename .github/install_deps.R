d3ddfccaed634fcc6b973dc9db598e2fe85a19bc
Tom Elliott <tomelliottnz@gmail.com>
2024-05-02 10:44:36 +1200

    update deps

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -1,17 +1,17 @@
 # R script
 github_deps <- c(
     "tmelliott/surveyspec",
-    "tmelliott/gWidgets2@patch-1",
+    # "tmelliott/gWidgets2@patch-1",
     "cran/maptools",
     "cran/rgdal",
     "cran/rgeos",
     "iNZightVIT/gWidgets2RGtk2@inz",
-    "iNZightVIT/iNZightTools@1.13.0",
-    "iNZightVIT/iNZightTS@1.5.8",
-    "iNZightVIT/iNZightMR@2.2.7",
+    "iNZightVIT/iNZightTools@2.0.1",
+    "iNZightVIT/iNZightTS@1.5.10",
+    "iNZightVIT/iNZightMR@2.3.0",
     "iNZightVIT/iNZightPlots@2.15.0",
     "iNZightVIT/iNZightRegression@1.3.3",
-    "iNZightVIT/iNZight@4.3.0",
+    "iNZightVIT/iNZight@4.4.0",
     "iNZightVIT/iNZightMaps@2.3"
 )
 
