f6f04ae409aebfbbcf93aa58bfa1bd628bc21219
Tom Elliott <tomelliottnz@gmail.com>
2025-04-08 15:04:59 +1200

    github ci updates

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -1,36 +1,18 @@
 # R script
-needs <- c(
-    "tmelliott/surveyspec",
-    # "tmelliott/gWidgets2@patch-1",
-    # "cran/Matrix@1.6-5",
-    # "cran/MatrixModels@0.5-2",
-    # "cran/maptools",
-    # "cran/rgdal",
-    # "cran/rgeos",
-    "iNZightVIT/gWidgets2RGtk2@inz",
-    "iNZightVIT/iNZightTools@2.0.1",
-    "iNZightVIT/iNZightTS@1.5.10", # aka iNZightTSLegacy
-    "iNZightVIT/iNZightMR@2.3.0",
-    "iNZightVIT/iNZightPlots@2.15.0",
-    "iNZightVIT/iNZightRegression@1.3.3",
-    "iNZightVIT/iNZight@4.4.0"
-    # "iNZightVIT/iNZightMaps@2.3"
-)
-
-# OS <- Sys.getenv("OS_TYPE")
-# options(
-#     repos = c(
-#         # if (OS == "Linux") RSPM <- Sys.getenv("RSPM"),
-#         inzight = "https://r.docker.stat.auckland.ac.nz",
-#         CRAN = "https://cloud.r-project.org"
-#     )
-#     # install.packages.compile.from.source = "never"
-# )
-
 if (!requireNamespace("pak", quietly = TRUE)) {
     install.packages("pak", type = "source")
 }
 
+# split by ,\n
+needs <- strsplit(
+    read.dcf("DESCRIPTION")[1, "Config/Needs/dependencies"],
+    ",\n"
+)[[1]]
+
+# drop RGtk2
+needs <- needs[!grepl("RGtk2$", needs)]
+
+pak::repo_add("https://r.docker.stat.auckland.ac.nz")
 
 if (Sys.getenv("OS_TYPE") == "Windows" &&
     getRversion() < numeric_version("4.4")) {
@@ -41,39 +23,7 @@ if (Sys.getenv("OS_TYPE") == "Windows" &&
     )
 }
 
-
-# pak::pak(github_deps, dependencies = TRUE)
-# pak::local_install_dev_deps(dependencies = TRUE, upgrade = FALSE)
 pak::pak(c(".", needs, "iNZightMaps=?ignore"),
     dependencies = TRUE
 )
 pak::pak("rcmdcheck")
-
-# if (OS == "Linux" &&
-#     !requireNamespace("units", quietly = TRUE) &&
-#     getRversion() >= "4.3") {
-#     system("sudo apt-get install libudunits2-dev")
-# }
-
-# if (OS == "Windows" && getRversion() < numeric_version("4")) {
-#     install.packages("RODBC", type = "binary")
-#     install.packages("rgl", type = "binary")
-# }
-# if (OS == "Windows") {
-#     dir.create(tools::R_user_dir("iNZight", "config"), recursive = TRUE)
-# }
-
-# install broom.helpers if not already installed
-# if (!requireNamespace("broom.helpers", quietly = TRUE)) {
-#     install.packages("broom.helpers", repos = "https://cloud.r-project.org")
-# }
-
-# remotes::install_github(
-#     github_deps,
-#     INSTALL_opts = "--no-multiarch"
-# )
-# remotes::install_deps(
-#     dependencies = TRUE,
-#     INSTALL_opts = "--no-multiarch"
-# )
-# remotes::install_cran("rcmdcheck")
